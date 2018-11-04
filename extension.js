5fdf21ce608a7df6b9b93ea907dc2364610d1f7e
xnng <xnng77@gmail.com>
2018-11-04 23:14:04 +0800

    add command

--- extension.js
+++ extension.js
@@ -13,18 +13,17 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    let format = vscode.commands.registerCommand('extension.format', function() {
+        vscode.window.showInformationMessage('format');
+    });
+    let resetFormat = vscode.commands.registerCommand('extension.resetFormat', function() {
+        vscode.window.showInformationMessage('resetFormat');
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(format, resetFormat);
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {
-}
+function deactivate() {}
 exports.deactivate = deactivate;
