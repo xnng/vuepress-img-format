c6ecff0500d31ddd10ed8fbc5b52666958fc9ed5
xnng <xnng77@gmail.com>
2018-11-05 16:49:15 +0800

    create newImg

--- extension.js
+++ extension.js
@@ -1,21 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vuepress-img-format" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     let format = vscode.commands.registerCommand('extension.format', function() {
+        let originText = vscode.window.activeTextEditor.document.getText()
+        let imgRegex = /\!\[[^\[\]]*\]\([^\(\)]*\)(?!<\/a>)/
+        let imgCode = imgRegex.exec(originText)
+
+        let title = imgCode[0].substring(imgCode[0].indexOf('[') + 1, imgCode[0].indexOf(']'))
+        let url = imgCode[0].substring(imgCode[0].indexOf('(') + 1, imgCode[0].indexOf(')'))
+        let newImg = `<a data-fancybox title="${title}" href="${url}">${imgCode}</a>`
+
+        console.log(newImg)
+
         vscode.window.showInformationMessage('format');
     });
+
     let resetFormat = vscode.commands.registerCommand('extension.resetFormat', function() {
         vscode.window.showInformationMessage('resetFormat');
     });
@@ -24,6 +23,5 @@ function activate(context) {
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {}
-exports.deactivate = deactivate;
+exports.deactivate = deactivate
