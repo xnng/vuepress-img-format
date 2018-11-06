d0f07339b3fe070d8ee36535feb753f286299ef8
xnng <xnng77@gmail.com>
2018-11-06 11:50:27 +0800

    all works done

--- extension.js
+++ extension.js
@@ -4,36 +4,93 @@ function activate(context) {
     let format = vscode.commands.registerCommand('extension.format', function() {
         let originText = vscode.window.activeTextEditor.document.getText()
 
-        let imgRegex = /\!\[[^\[\]]*\]\([^\(\)]*\)(?!<\/a>)/g
-        let imgCode = imgRegex.exec(originText)
+        // get all image code without <a></a>
+        let getAllImgCodeRegex = /\!\[[^\[\]]*\]\([^\(\)]*\)(?!<\/a>)/g
+        let imgCode = originText.match(getAllImgCodeRegex)
 
-        let title = imgCode[0].substring(imgCode[0].indexOf('[') + 1, imgCode[0].indexOf(']'))
-        let url = imgCode[0].substring(imgCode[0].indexOf('(') + 1, imgCode[0].indexOf(')'))
-        let newImgCode = `<a data-fancybox title="${title}" href="${url}">${imgCode}</a>`
+        if (imgCode) {
+            let newImgCode = imgCode.map((item) => {
+                let title = item.substring(item.indexOf('[') + 1, item.indexOf(']'))
+                let url = item.substring(item.indexOf('(') + 1, item.indexOf(')'))
+                let newItem = `<a data-fancybox title="${title}" href="${url}">${item}</a>`
+                return newItem
+            })
 
-        let newText = originText.replace(imgCode[0], newImgCode)
+            // replace all image code
+            let newText
+            let dynamicText = originText
+            let getOneImgCodeRegex = /\!\[[^\[\]]*\]\([^\(\)]*\)(?!<\/a>)/
+            for (let i = 0; i < imgCode.length; i++) {
+                newText = dynamicText.replace(getOneImgCodeRegex, newImgCode[i])
+                dynamicText = newText
+            }
 
-        // get the Range of the whole text of a document
-        let textEditor = vscode.window.activeTextEditor
-        let firstLine = textEditor.document.lineAt(0);
-        let lastLine = textEditor.document.lineAt(textEditor.document.lineCount - 1);
-        let textRange = new vscode.Range(0,
-            firstLine.range.start.character,
-            textEditor.document.lineCount - 1,
-            lastLine.range.end.character);
+            // get the Range of the whole text of a document
+            let textEditor = vscode.window.activeTextEditor
+            let firstLine = textEditor.document.lineAt(0);
+            let lastLine = textEditor.document.lineAt(textEditor.document.lineCount - 1);
+            let textRange = new vscode.Range(0,
+                firstLine.range.start.character,
+                textEditor.document.lineCount - 1,
+                lastLine.range.end.character);
 
-        let uri = vscode.window.activeTextEditor.document.uri
-        let textEdits = [];
-        textEdits.push(new vscode.TextEdit(textRange, newText))
-        let workspaceEdit = new vscode.WorkspaceEdit()
-        workspaceEdit.set(uri, textEdits)
-        vscode.workspace.applyEdit(workspaceEdit)
-
-        vscode.window.showInformationMessage('format successful');
+            // Replace documents with vscode API
+            let uri = vscode.window.activeTextEditor.document.uri
+            let textEdits = [];
+            textEdits.push(new vscode.TextEdit(textRange, newText))
+            let workspaceEdit = new vscode.WorkspaceEdit()
+            workspaceEdit.set(uri, textEdits)
+            vscode.workspace.applyEdit(workspaceEdit)
+            vscode.window.showInformationMessage('Formatting success.');
+        } else {
+            vscode.window.showInformationMessage('Formatting has been completed.');
+        }
     });
 
     let resetFormat = vscode.commands.registerCommand('extension.resetFormat', function() {
-        vscode.window.showInformationMessage('resetFormat');
+        let originText = vscode.window.activeTextEditor.document.getText()
+
+        // get all image code without <a></a>
+        let getAllImgCodeRegex = /<a[^<]*(?=<\/a>)<\/a>/g
+        let imgCode = originText.match(getAllImgCodeRegex)
+
+        if (imgCode) {
+
+            let newImgCode = imgCode.map((item) => {
+                let newItem = item.substring(item.indexOf('!'), item.indexOf(')') + 1)
+                return newItem
+            })
+
+            // replace all image code
+            let newText
+            let dynamicText = originText
+            let getOneImgCodeRegex = /<a[^<]*(?=<\/a>)<\/a>/
+            for (let i = 0; i < imgCode.length; i++) {
+                newText = dynamicText.replace(getOneImgCodeRegex, newImgCode[i])
+                dynamicText = newText
+            }
+
+            // get the Range of the whole text of a document
+            let textEditor = vscode.window.activeTextEditor
+            let firstLine = textEditor.document.lineAt(0);
+            let lastLine = textEditor.document.lineAt(textEditor.document.lineCount - 1);
+            let textRange = new vscode.Range(0,
+                firstLine.range.start.character,
+                textEditor.document.lineCount - 1,
+                lastLine.range.end.character);
+
+            // Replace documents with vscode API
+            let uri = vscode.window.activeTextEditor.document.uri
+            let textEdits = [];
+            textEdits.push(new vscode.TextEdit(textRange, newText))
+            let workspaceEdit = new vscode.WorkspaceEdit()
+            workspaceEdit.set(uri, textEdits)
+            vscode.workspace.applyEdit(workspaceEdit)
+            vscode.window.showInformationMessage('Reset formatting success');
+        } else {
+            vscode.window.showInformationMessage('Reset formatting completed');
+        }
+
     });
 
     context.subscriptions.push(format, resetFormat);
